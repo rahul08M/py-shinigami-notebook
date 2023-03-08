f533597b3dd6616d7bcbd3a6702c8eb2f6660225
Rahul Mohite <41115652+rahul08M@users.noreply.github.com>
2023-03-08 17:45:45 +0530

    Update connection.py

--- mongodb/connection.py
+++ mongodb/connection.py
@@ -1,1 +1,8 @@
+# Here's an example of how to use PyMongo to connect to a MongoDB instance:
 
+import pymongo
+
+
+def client():
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    return client
