f9b5a6d58f77cf41c24e994cea2413f2f1438b54
Rahul Mohite <41115652+rahul08M@users.noreply.github.com>
2023-03-14 14:41:50 +0530

    Update mongo_read.py

--- mongodb/mongo_read.py
+++ mongodb/mongo_read.py
@@ -1,1 +1,7 @@
+import pymongo
 
+client = pymongo.MongoClient('mongodb://localhost:27017/')
+db = client['ninja']
+
+db.coll.find_one()    # returns a single document
+db.coll.find()    # returns a cursor - show 20 results - "it" to display more
