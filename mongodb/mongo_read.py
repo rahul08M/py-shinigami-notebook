e2f313fbf21cb117634854f4170df538378d9f9f
Rahul Mohite <41115652+rahul08M@users.noreply.github.com>
2023-03-15 17:17:32 +0530

    Update mongo_read.py

--- mongodb/mongo_read.py
+++ mongodb/mongo_read.py
@@ -6,4 +6,3 @@ db = client['ninja']
 db.coll.find_one()    # returns a single document
 db.coll.find()    # returns a cursor - show 20 results - "it" to display more
 db.coll.find({'shinobi_name': 'Kakashi', 'shinobi_age': 20})    # implicit logical "AND".
-
