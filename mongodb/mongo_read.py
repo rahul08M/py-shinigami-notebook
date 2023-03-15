02175a2d54cabe452e0ce64eed06557466251d25
Rahul Mohite <41115652+rahul08M@users.noreply.github.com>
2023-03-15 18:49:05 +0530

    Update mongo_read.py

--- mongodb/mongo_read.py
+++ mongodb/mongo_read.py
@@ -6,3 +6,5 @@ db = client['ninja']
 db.coll.find_one()    # returns a single document
 db.coll.find()    # returns a cursor - show 20 results - "it" to display more
 db.coll.find({'shinobi_name': 'Kakashi', 'shinobi_age': 20})    # implicit logical "AND".
+
+db.coll.find({'shinobi_age': {'$gt': 20}})   #  $gt selects those documents where the value of the field is greater than (i.e. >) the specified value
